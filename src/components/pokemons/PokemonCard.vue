47f3618a030beb1529470c1d291bf9f84da34172
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-02-05 18:48:37 +0100

    added PokemonCard layout

--- src/components/pokemons/PokemonCard.vue
+++ src/components/pokemons/PokemonCard.vue
@@ -4,6 +4,17 @@ export default {
 }
 </script>
 
-<template></template>
+<template>
+    <div class="col">
+        <div class="card">
+            <img src="" class="card-img-top" alt="">
+            <div class="card-body text-center">
+                <span></span>
+                <h4 class="card-title"></h4>
+                <p class="card-text"></p>
+            </div>
+        </div>
+    </div>
+</template>
 
 <style></style>
