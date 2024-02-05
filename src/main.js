615420ba74a6f3ce768fd5cfc678e5abaea41295
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-02-05 17:29:10 +0100

    install & import FontAwesome

--- src/main.js
+++ src/main.js
@@ -4,4 +4,21 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+/* Importo la libreria generica */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* Importo il componente di Font Awesome */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* Importo l'icona che voglio usare */
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+
+/* Icone da caricare */
+library.add(faMagnifyingGlass);
+
+
+const app = createApp(App)
+
+app.component('FontAwesomeIcon', FontAwesomeIcon)
+
+app.mount('#app')
