837fc05fb7a2797501e25916557bfd9630a1df82
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-02-05 19:16:01 +0100

    added pokemon element

--- src/components/pokemons/PokemonCard.vue
+++ src/components/pokemons/PokemonCard.vue
@@ -1,18 +1,22 @@
 <script>
 export default {
     name: 'PokemonCard',
+    props: {
+        type: String,
+        number: Number,
+        name: String,
+        url: String
+    }
 }
 </script>
 
 <template>
-    <div class="col">
-        <div class="card">
-            <img src="" class="card-img-top" alt="">
-            <div class="card-body text-center">
-                <span></span>
-                <h4 class="card-title"></h4>
-                <p class="card-text"></p>
-            </div>
+    <div class="card text-center my-4">
+        <img :src="url" class="card-img-top mt-3" :alt="name">
+        <div class="card-body">
+            <span>{{ number }}</span>
+            <h4 class="card-title">{{ name }}</h4>
+            <p class="card-text">{{ type }}</p>
         </div>
     </div>
 </template>
--- src/components/pokemons/PokemonsList.vue
+++ src/components/pokemons/PokemonsList.vue
@@ -1,7 +1,9 @@
 <script>
+import { store } from '../../data/store.js';
 import PokemonCard from './PokemonCard.vue';
 export default {
     name: 'PokemonsList',
+    data: () => ({ store }),
     components: { PokemonCard }
 }
 </script>
@@ -10,7 +12,9 @@ export default {
     <!-- Section Pokemons Cards -->
     <section id="pokemons-cards">
         <div class="row row-cols-5 g-4">
-            <PokemonCard />
+            <div class="col" v-for="pokemon in store.pokemons" :key="pokemon.id">
+                <PokemonCard :type="pokemon.type1" :name="pokemon.name" :number="pokemon.number" :url="pokemon.imageUrl" />
+            </div>
         </div>
     </section>
 </template>
