1b20a6f4bcc4368c01185d940a41ff69417f0b96
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-02-05 17:35:12 +0100

    added header

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+    name: 'AppHeader'
+}
+</script>
+
+<template>
+    <header>
+        <div class="container d-flex justify-content-between mt-5 mb-3">
+            <h1>Pokédex Nazionale</h1>
+            <FontAwesomeIcon icon="fa-solid fa-magnifying-glass" class="fa-2x" />
+        </div>
+    </header>
+</template>
+
+<style></style>
