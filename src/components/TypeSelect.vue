c76039bdcc39d383028c1a78df875b5627c23acc
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-02-06 17:20:56 +0100

    created TypeSelect

---
+++ src/components/TypeSelect.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+    name: 'TypeSelect'
+}
+</script>
+
+<template>
+    <select class="form-select me-3">
+        <option selected>All</option>
+        <option value="1">One</option>
+        <option value="2">Two</option>
+        <option value="3">Three</option>
+    </select>
+</template>
+
+<style></style>
