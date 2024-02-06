5fdd9bb87ee865cc1be11d00d4dedef00604f4db
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-02-06 16:51:41 +0100

    fixed align items

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -6,7 +6,7 @@ export default {
 
 <template>
     <header>
-        <div class="container d-flex justify-content-between mt-5 mb-4">
+        <div class="container d-flex justify-content-between align-items-center mt-5 mb-4">
             <h1 class="ms-3">Pokédex Nazionale</h1>
             <FontAwesomeIcon icon="fa-solid fa-magnifying-glass" class="fa-2x me-3" role="button" />
         </div>
