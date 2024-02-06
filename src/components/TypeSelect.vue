01bc843e7fec39a7956721041ae0149c4a626a12
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-02-06 18:47:33 +0100

    added emits & @click

--- src/components/TypeSelect.vue
+++ src/components/TypeSelect.vue
@@ -23,12 +23,13 @@ export default {
             "Water"
         ],
         selectOption: 'All'
-    })
+    }),
+    emits: ['option-select'],
 }
 </script>
 
 <template>
-    <select class="form-select me-3" v-model="selectOption">
+    <select class="form-select me-3" v-model="selectOption" @click="$emit('option-select', selectOption)">
         <option selected>All</option>
         <option v-for="(type, i) in types" :key="i" :value="type">{{ type }}</option>
     </select>
