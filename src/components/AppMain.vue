79b04e0617e2237847b85d7823d2845788c6ea58
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-02-07 00:21:20 +0100

    added emits

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -3,13 +3,14 @@ import PokemonsList from './pokemons/PokemonsList.vue';
 import MainButtons from './MainButtons.vue';
 export default {
     name: 'AppMain',
-    components: { PokemonsList, MainButtons }
+    components: { PokemonsList, MainButtons },
+    emits: ['more-pokemons', 'reset']
 }
 </script>
 
 <template>
     <main class="container">
         <PokemonsList />
-        <MainButtons />
+        <MainButtons @more-pokemons="$emit('more-pokemons')" @reset="$emit('reset')" />
     </main>
 </template>
