16c54c2f9fbcf3dc0bb610bcd847cec0ba8a59b5
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-02-07 00:05:25 +0100

    import MainButtons in AppMain

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,13 +1,15 @@
 <script>
 import PokemonsList from './pokemons/PokemonsList.vue';
+import MainButtons from './MainButtons.vue';
 export default {
     name: 'AppMain',
-    components: { PokemonsList }
+    components: { PokemonsList, MainButtons }
 }
 </script>
 
 <template>
     <main class="container">
         <PokemonsList />
+        <MainButtons />
     </main>
 </template>
