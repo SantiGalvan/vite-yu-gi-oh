abd28a4e144afd5144525ff8173b8a55a5d7a863
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-02-07 01:01:53 +0100

    created new emits

--- src/components/SearchForm.vue
+++ src/components/SearchForm.vue
@@ -3,12 +3,12 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 export default {
     name: 'SearchForm',
     components: { FontAwesomeIcon },
-    emits: ['get-input']
+    emits: ['get-input', 'submit-form']
 }
 </script>
 
 <template>
-    <form @submit.prevent="">
+    <form @submit.prevent="$emit('submit-form')">
         <div class="d-flex align-items-center ms-3">
             <input type="text" class="form-control" placeholder="Cerca Pokemon">
             <FontAwesomeIcon icon="fa-solid fa-xmark" class="fa-2x ms-2" role="button" @click="$emit('get-input')" />
