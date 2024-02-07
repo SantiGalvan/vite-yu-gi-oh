91496b5101613f3938550746e3a28acd650e8ad3
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-02-07 01:49:32 +0100

    added searchterm in parameters

--- src/components/SearchForm.vue
+++ src/components/SearchForm.vue
@@ -14,7 +14,7 @@ export default {
 </script>
 
 <template>
-    <form @submit.prevent="$emit('submit-form')">
+    <form @submit.prevent="$emit('submit-form', searchTerm)">
         <div class="d-flex align-items-center ms-3">
             <input type="text" class="form-control" :placeholder="placeholder" v-model.trim="searchTerm">
             <FontAwesomeIcon icon="fa-solid fa-xmark" class="fa-2x ms-2" role="button" @click="$emit('get-input')" />
