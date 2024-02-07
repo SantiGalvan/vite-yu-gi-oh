3755ae049ec0342dba6ae9f0d328b9b7a6e4b124
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-02-07 01:00:24 +0100

    import SearchForm & added new methods getInput & created new data isSearch

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,10 +1,17 @@
 <script>
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { store } from '../data/store.js';
 import TypeSelect from './TypeSelect.vue';
+import SearchForm from './SearchForm.vue'
 export default {
     name: 'AppHeader',
-    data: () => ({ store }),
-    components: { TypeSelect },
+    data: () => ({ store, isSearch: false }),
+    methods: {
+        getInput() {
+            this.isSearch = !this.isSearch;
+        }
+    },
+    components: { TypeSelect, FontAwesomeIcon, SearchForm },
     emits: ['option-select']
 }
 </script>
@@ -14,7 +21,9 @@ export default {
         <div class="container text-center mt-3">
             <h1 class="ms-3 fw-bolder">Pokédex Nazionale</h1>
             <div class="d-flex justify-content-between align-items-center">
-                <FontAwesomeIcon icon="fa-solid fa-magnifying-glass" class="fa-2x ms-3" role="button" />
+                <FontAwesomeIcon v-if="!isSearch" icon="fa-solid fa-magnifying-glass" class="fa-2x ms-3" role="button"
+                    @click="getInput" />
+                <SearchForm v-else @get-input="getInput" />
                 <TypeSelect @option-select="$emit('option-select', $event)" :types="store.types" />
             </div>
         </div>
