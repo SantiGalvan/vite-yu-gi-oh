4461d9bf86f68ee573428f9f68ca88ace465c0de
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-02-07 01:14:33 +0100

    added props

--- src/components/SearchForm.vue
+++ src/components/SearchForm.vue
@@ -3,6 +3,9 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 export default {
     name: 'SearchForm',
     components: { FontAwesomeIcon },
+    props: {
+        placeholder: String
+    },
     emits: ['get-input', 'submit-form']
 }
 </script>
@@ -10,7 +13,7 @@ export default {
 <template>
     <form @submit.prevent="$emit('submit-form')">
         <div class="d-flex align-items-center ms-3">
-            <input type="text" class="form-control" placeholder="Cerca Pokemon">
+            <input type="text" class="form-control" :placeholder="placeholder">
             <FontAwesomeIcon icon="fa-solid fa-xmark" class="fa-2x ms-2" role="button" @click="$emit('get-input')" />
         </div>
     </form>
