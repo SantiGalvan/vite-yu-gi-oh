b254821eeafa37ddf812a22c7c71dce796952393
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-02-07 11:58:27 +0100

    fixed v-on

--- src/components/TypeSelect.vue
+++ src/components/TypeSelect.vue
@@ -12,7 +12,7 @@ export default {
 </script>
 
 <template>
-    <select class="form-select me-3" v-model="selectOption" @click="$emit('option-select', selectOption)">
+    <select class="form-select me-3" v-model="selectOption" @change="$emit('option-select', selectOption)">
         <option selected>All</option>
         <option v-for="(type, i) in types" :key="i" :value="type">{{ type }}</option>
     </select>
