98d6413c7b44506373d721d7a688dd5fa99a7cf2
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-02-07 01:07:36 +0100

    remove isSearch & fixed method

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -5,10 +5,10 @@ import TypeSelect from './TypeSelect.vue';
 import SearchForm from './SearchForm.vue'
 export default {
     name: 'AppHeader',
-    data: () => ({ store, isSearch: false }),
+    data: () => ({ store }),
     methods: {
         getInput() {
-            this.isSearch = !this.isSearch;
+            this.store.isSearch = !this.store.isSearch;
         }
     },
     components: { TypeSelect, FontAwesomeIcon, SearchForm },
@@ -21,9 +21,9 @@ export default {
         <div class="container text-center mt-3">
             <h1 class="ms-3 fw-bolder">Pokédex Nazionale</h1>
             <div class="d-flex justify-content-between align-items-center">
-                <FontAwesomeIcon v-if="!isSearch" icon="fa-solid fa-magnifying-glass" class="fa-2x ms-3" role="button"
+                <FontAwesomeIcon v-if="!store.isSearch" icon="fa-solid fa-magnifying-glass" class="fa-2x ms-3" role="button"
                     @click="getInput" />
-                <SearchForm v-else @get-input="getInput" />
+                <SearchForm v-else @get-input="getInput" @submit-form="console.log('cliccato')" />
                 <TypeSelect @option-select="$emit('option-select', $event)" :types="store.types" />
             </div>
         </div>
